e299483be6ba244c5baf23c4a4d3dbc617e427ef
phatpham9 <phatpham9@gmail.com>
2020-08-24 23:01:20 +0700

    fix(user): wrong redirect paths

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -14,19 +14,19 @@ const routes = (_: RouteOptions): Router => {
   const router = Router();
 
   router.route('/').get(authenticate, (req, res) => {
-    res.redirect(307, `${users.path}/${(req.user as string) ?? ''}`);
+    res.redirect(307, `/api${users.path}/${(req.user as string) ?? ''}`);
   });
 
   router.route(sessions.path).all((_, res) => {
-    res.redirect(307, sessions.path);
+    res.redirect(307, `/api${sessions.path}`);
   });
 
   router.route(`${sessions.path}/current`).all(authenticate, (req, res) => {
-    res.redirect(307, `${sessions.path}/${req.sessionID ?? ''}`);
+    res.redirect(307, `/api${sessions.path}/${req.sessionID ?? ''}`);
   });
 
   router.route(rbac.path).get(authenticate, (req, res) => {
-    res.redirect(307, `${rbac.path}/${(req.user as string) ?? ''}`);
+    res.redirect(307, `/api${rbac.path}/${(req.user as string) ?? ''}`);
   });
 
   return router;
